f6a5ead34f866ed7ac260db1c1474d70985066c6
Roman-Ceban <72251111+Roman-Ceban@users.noreply.github.com>
2020-10-06 21:43:05 +0300

    Upload console project

---
+++ 1111111/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        User user1 = new User("Roman", "Ceban", 25, "Roma00952011@mail.ru", status.ACTIVE, LocalDateTime.now());
+        User user2 = new User("Elena", "Ceban", 22, "Roma00952011@mail.ru", status.NEW, LocalDateTime.parse("2019-11-09 10:30", formatter));
+        User user3 = new User("Mxim", "Negru", 23, "Roma00952011@mail.ru", status.INACTIVE, LocalDateTime.now());
+        User user4 = new User("Valeriu", "Albu", 28, "Roma00952011@mail.ru", status.NEW, LocalDateTime.parse("2020-10-01 11:30", formatter));
+        User user5 = new User("Dorina", "Verde", 26, "Roma00952011@mail.ru", status.ACTIVE, LocalDateTime.now());
+
+        ArrayList<User> usersList = new ArrayList<User>();
+        usersList.add(user1);
+        usersList.add(user2);
+        usersList.add(user3);
+        usersList.add(user4);
+        usersList.add(user5);
+
+        listUsers(usersList);
+    ///    activateUsersOlderThanYesterday(usersList);
+        activateUsersOlderThanOneMonth(usersList);
+        listUsers(usersList);
+
+
+    }
+    private static void activateUsersOlderThanOneMonth(ArrayList<User> users){
+        LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
+        for (User user : users) {
+            if (user.getstatus() == status.NEW && user.getTimestamp().isBefore(oneMonthAgo)){
+                user.setStatus(status.BLOCKED);
+            }
+        }
+    }
+
+    private static void activateUsersOlderThanYesterday(ArrayList<User> users) {
+       LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        for (User user : users) {
+           if (user.getstatus() == status.NEW && user.getTimestamp().isBefore(yesterday)){
+                user.setStatus(status.ACTIVE);
+            }
+        }
+    }
+
+    private static void listUsers(ArrayList<User> users) {
+        System.out.println("-----------");
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
+    }
+}
+
---
+++ 1111111/src/com/company/User.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+//clasa
+public class User {
+    private String lastname;
+    private String firstname;
+    private int age;
+    private String email;
+    private status status;
+    private LocalDateTime timestamp;
+
+    //constructor
+    public User(String firstname, String lastname, int age, String email, status status, LocalDateTime localDateTime) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.email = email;
+        this.status = status;
+        this.timestamp = localDateTime;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+
+    }
+    public int getage() {
+        return age;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public status getstatus(){
+        return status;
+
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+
+    /// de adus toate  geter
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setStatus(status status){
+        this.status = status;
+    }
+
+    public void setTimestamp(LocalDateTime dateTime){
+        this.timestamp = dateTime;
+    }
+
+       // de facut pentru toate seter
+    @Override
+    public String toString() {
+        return String.format(firstname + " " + lastname + " " + age + " " + status);
+    }
+}
+
+//Enum--> Valori constante
+enum status {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED,
+    NEW
+}
